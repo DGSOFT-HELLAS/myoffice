f0129e9249560616a80172941cd4fa0b7ac88d1d
ccm <ccm_chiout@ccms-Mac-mini.local>
2023-07-06 15:54:41 +0300

    pod install

---
+++ App.js
@@ -0,0 +1,46 @@
+
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { StatusBar } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import { Layout } from './src/useContext/useContect';
+import { DaysContext } from './src/useContext/daysContext';
+import DrawerNav from './src/navigation';
+
+
+function App() {
+  return (
+    <>
+      <StatusBar backgroundColor={'#000000'} />
+      <Layout>
+        <DaysContext>
+          <DrawerNav />
+        </DaysContext>
+      </Layout>
+    </>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
--- ios/myofficeappTests/myofficeappTests.m
+++ ios/myofficeappTests/myofficeappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface myDataTests : XCTestCase
+@interface myofficeappTests : XCTestCase
 
 @end
 
-@implementation myDataTests
+@implementation myofficeappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
