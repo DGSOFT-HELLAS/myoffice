6990e6cf2851c7e2c0d787a480eb01a808bcdf08
Dimitris Gounaris <dimitrisgnrs9@gmail.com>
2026-04-28 18:31:24 +0300

    Update project dependencies and configurations for React Native 0.85.2; adjust ESLint and Prettier settings, enhance Android build settings, and remove unused files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: '@react-native',
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-dupe-keys': 'warn',
+    'no-undef': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
 
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  StatusBar,
-  StyleSheet,
-} from 'react-native';
+import {StatusBar} from 'react-native';
 
 import { Layout } from './src/useContext/useContect';
 import { DaysContext } from './src/useContext/daysContext';
 import DrawerNav from './src/navigation';
 
 
-function App(): JSX.Element {
+function App(): React.JSX.Element {
   return (
     <>
       <StatusBar backgroundColor={'#000000'} />
@@ -24,24 +21,5 @@ function App(): JSX.Element {
 
   )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
